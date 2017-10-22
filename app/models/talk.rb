e2e4d64ee59648e2495d0f5dc0f5eddc67e64c6d
TaigaK <backnum5509@gmail.com>
2017-10-23 04:40:01 +0900

    update:optional

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,6 @@
 class Talk < ApplicationRecord
   belongs_to :family
-  belongs_to :hasband,  foreign_key: "user_id"
-  belongs_to :wife,     foreign_key: "user_id"
+  belongs_to :hasband,  foreign_key: "user_id", optional: true
+  belongs_to :wife,     foreign_key: "user_id", optional: true
   belongs_to :user, :polymorphic => true
 end
